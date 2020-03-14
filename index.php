1f397ca06cffe76bba82ef77d5dfe0d237944d03
shaakilahammed <60335472+shaakilahammed@users.noreply.github.com>
2020-03-14 15:55:48 +0600

    Lab-7

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>XCOMPANY</title>
+	<style>
+		body{
+			margin:0;
+			padding:0px 50px;
+		}
+		a{
+			text-decoration:none;
+		}
+		.header_area{
+			width:100%;
+			
+			
+		}
+		.logoarea{
+			width:40%;
+			float:left;
+			background-color:#25cee0;
+		}
+		.logoarea h1{
+			padding-left:30px;
+		}
+		.menu_area{
+			width:60%;
+			float:left;
+			background-color:#25cee0;
+		}
+		.menu_area ul{
+			list-style:none;
+			text-align:right;
+		}
+		.menu_area ul li{
+			display:inline-block;
+			padding:15px;
+			color:white;
+		}
+		.menu_area ul li a{
+			
+			color:white;
+		}
+		.content_area{
+			height:500px;
+			width:100%;
+			overflow:hidden;
+		}
+		
+		.footer_area{
+			width:100%;
+			overflow:hidden;
+		}
+		.footer_area h3{
+			text-align:center;
+		}
+	</style>
+</head>
+<body>
+	<div class="header_area">
+		<div class="logoarea">
+			<h1><span class="x">X</span>Company</h1>
+		</div>
+		<div class="menu_area">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="login.php">Login</a></li>
+				<li><a href="registration.php">Registration</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="content_area">
+		<h1>Welcome to Our Company</h1>
+	</div>
+	<div class="footer_area">
+		<h3>&copy; all right reserved Shakil Ahammed</h3>
+	</div>
+</body>
+</html>
